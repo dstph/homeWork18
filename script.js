38e828ccdcced483bc708f02fb7c99089b31db2f
dstph <genyadstph@gmail.com>
2020-08-20 13:53:01 +0400

    method all

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 
 
-let twoArrays = new Promise(( response, reject ) => {
-	let xhr = new XMLHttpRequest();
-	xhr.open('GET', 'data.json');
-	xhr.send();
-	xhr.addEventListener('readystatechange', () => {
-		if(xhr.readyState === 4){
-			if(xhr.status <= 400){
-				firstArr = JSON.parse(xhr.responseText);
-				console.log('first xhr', firstArr)
-				firstArr.length !== 0 ? response(firstArr) : reject(firstArr);
-			}
-		}
-	});
-})
+// let twoArrays = new Promise(( response, reject ) => {
+// 	let xhr = new XMLHttpRequest();
+// 	xhr.open('GET', 'data.json');
+// 	xhr.send();
+// 	xhr.addEventListener('readystatechange', () => {
+// 		if(xhr.readyState === 4){
+// 			if(xhr.status <= 400){
+// 				firstArr = JSON.parse(xhr.responseText);
+// 				console.log('first xhr', firstArr)
+// 				firstArr.length !== 0 ? response(firstArr) : reject(firstArr);
+// 			}
+// 		}
+// 	});
+// })
 
-twoArrays
-		.then(
-			()=>{
-				return new Promise((responseTwo, rejectTwo)=>{
-					let xhrTwo = new XMLHttpRequest();
-					xhrTwo.open('GET', 'data2.json');
-					xhrTwo.send();
-					xhrTwo.addEventListener('readystatechange', () => {
-						if(xhrTwo.readyState === 4){
-							if(xhrTwo.status <= 400){
-								secondArr = JSON.parse(xhrTwo.responseText);
-								console.log('second xhr', secondArr);
-								secondArr.length !==0 ? responseTwo(secondArr) : rejectTwo(secondArr);
-							}
-						}
-					})
-				})
-			}
-		)
-		.then(
-			(data)=>{
-				thirdArr = firstArr.concat(data);
-				console.log(thirdArr);
-			}
-		)
-		.catch(
-			()=>{
-				console.log('error');
-			}
-		)
-		.finally(
-			()=>{
-				console.log('I did it!');
-			}
-		)
+// twoArrays
+// 		.then(
+// 			()=>{
+// 				return new Promise((responseTwo, rejectTwo)=>{
+// 					let xhrTwo = new XMLHttpRequest();
+// 					xhrTwo.open('GET', 'data2.json');
+// 					xhrTwo.send();
+// 					xhrTwo.addEventListener('readystatechange', () => {
+// 						if(xhrTwo.readyState === 4){
+// 							if(xhrTwo.status <= 400){
+// 								secondArr = JSON.parse(xhrTwo.responseText);
+// 								console.log('second xhr', secondArr);
+// 								secondArr.length !==0 ? responseTwo(secondArr) : rejectTwo(secondArr);
+// 							}
+// 						}
+// 					})
+// 				})
+// 			}
+// 		)
+// 		.then(
+// 			(data)=>{
+// 				thirdArr = firstArr.concat(data);
+// 				console.log(thirdArr);
+// 			}
+// 		)
+// 		.catch(
+// 			()=>{
+// 				console.log('error');
+// 			}
+// 		)
+// 		.finally(
+// 			()=>{
+// 				console.log('I did it!');
+// 			}
+// 		)
 
 
 let input = document.querySelector('#promise');
@@ -101,6 +101,37 @@ let inputSecond = document.querySelector('#promiseSecond');
 	})
 
 
+let getFile = (method, file) => {
+	return new Promise((resolve, reject) => {
+		let xhr = new XMLHttpRequest();
+		xhr.open(method,file,true);
+		xhr.send();
+
+		xhr.addEventListener('readystatechange', () => {
+			if(xhr.readyState === 4){
+				if(xhr.status <= 400){
+					resolve(JSON.parse(xhr.responseText));
+				}
+				else{
+					reject(xhr.statusText);
+				}
+			}
+		});
+	});
+};
+
+Promise
+	   .all([
+	   		getFile('GET', 'data.json'),
+	   		getFile('GET', 'data2.json')
+	   	])
+	   .then(
+	   		data => {
+	   			let newArr = data[0].concat(data[1]);
+	   			console.log ( `this it ${newArr}` );
+	   			return newArr;
+	   		}
+	   	)
 
 
 
